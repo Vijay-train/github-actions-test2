a155413e7212a7a55dea8f8b0277a29c5e00cab1
Vijayaraghavan Vedantham <vijayaraghavanvedantham@Vijayaraghavans-MacBook-Pro.local>
2022-07-05 08:18:12 +0530

    push wf

--- .github/Actions/Hello/dist/index.js
+++ .github/Actions/Hello/dist/index.js
@@ -1852,7 +1852,8 @@ const github = __webpack_require__(469);
 try {
   //   throw (new Error ("some error message"));
   const name = core.getInput("who-to-greet");
-  console.log("Hello ${name}");
+
+  console.log('Hello ${name}');
 
   const time = new Date();
   core.setOutput("time", time.toTimeString());
